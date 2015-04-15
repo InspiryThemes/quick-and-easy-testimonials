2af3a5bff46dcd88bf3ada2cf6a6da791dc22649
Muhammad Saqib Sarwar <saqibsarwar@gmail.com>
2015-04-15 13:27:59 +0500

    initial commit

---
+++ includes/class-quick-and-easy-testimonials-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://saqibsarwar.com/
+ * @since      1.0.0
+ *
+ * @package    Quick_And_Easy_Testimonials
+ * @subpackage Quick_And_Easy_Testimonials/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Quick_And_Easy_Testimonials
+ * @subpackage Quick_And_Easy_Testimonials/includes
+ * @author     M Saqib Sarwar <saqibsarwar@gmail.com>
+ */
+class Quick_And_Easy_Testimonials_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-quick-and-easy-testimonials-i18n.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       http://saqibsarwar.com/
+ * @since      1.0.0
+ *
+ * @package    Quick_And_Easy_Testimonials
+ * @subpackage Quick_And_Easy_Testimonials/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Quick_And_Easy_Testimonials
+ * @subpackage Quick_And_Easy_Testimonials/includes
+ * @author     M Saqib Sarwar <saqibsarwar@gmail.com>
+ */
+class Quick_And_Easy_Testimonials_i18n {
+
+	/**
+	 * The domain specified for this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $domain    The domain identifier for this plugin.
+	 */
+	private $domain;
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			$this->domain,
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+	/**
+	 * Set the domain equal to that of the specified domain.
+	 *
+	 * @since    1.0.0
+	 * @param    string    $domain    The domain that represents the locale of this plugin.
+	 */
+	public function set_domain( $domain ) {
+		$this->domain = $domain;
+	}
+
+}
